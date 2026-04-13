cc93dfd759283b5be088eb0b438d1f8f80124f62
이설아 <dlcodus1201@naver.com>
2026-04-13 10:16:14 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/README.md
+++ 프로그래머스/1/12928. 약수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 합 - 12928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928?language=java) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 0.17 ms
+메모리: 77.8 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 13일 10:13:50
+2026년 04월 13일 10:16:12
 
 ### 문제 설명
 
--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        
         int sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (n % arr[i] == 0) {
-                sum += arr[i];
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
             }
         }
         
