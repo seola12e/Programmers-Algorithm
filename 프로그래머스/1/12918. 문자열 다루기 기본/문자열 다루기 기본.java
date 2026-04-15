ea6dcbbb1eceaa97496c35708bd3ce6f4278c999
이설아 <dlcodus1201@naver.com>
2026-04-15 10:35:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return (s.length() == 4) || (s.length() == 6);
+    }
+}
