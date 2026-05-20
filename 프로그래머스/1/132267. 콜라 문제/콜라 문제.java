96ee0321f0d8a2c29a72271219255b7562c7264f
이설아 <dlcodus1201@naver.com>
2026-05-20 10:38:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public int solution(int a, int b, int n) {
+        
+        int sum = 0;
+        
+        while (n / a > 0) {
+            sum += n / a * b;
+            n = n / a * b + n % a;
+        }
+        
+        return sum;
+    }
+}
