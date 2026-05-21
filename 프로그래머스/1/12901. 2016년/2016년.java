6ae8520e2aff821a53d60460b0e9e9fa36fd1a36
이설아 <dlcodus1201@naver.com>
2026-05-21 10:31:17 +0900

    [level 1] Title: 2016년, Time: 0.85 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,14 +1,7 @@
+import java.time.LocalDate;
+
 class Solution {
     public String solution(int a, int b) {
-        
-        int[] last = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        String[] day = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
-        
-        int daySum = 0;
-        for (int i = 0; i < a-1; i++)
-            daySum += last[i];
-        daySum += b;
-        
-        return day[daySum % 7];
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
     }
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=java) 
 
 ### 성능 요약
 
-메모리: 60.8 MB, 시간: 0.02 ms
+메모리: 61 MB, 시간: 0.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 21일 10:24:51
+2026년 05월 21일 10:31:14
 
 ### 문제 설명
 
