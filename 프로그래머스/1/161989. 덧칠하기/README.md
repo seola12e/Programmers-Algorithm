724d6ce006b87e3f833fd61482ad660755023475
이설아 <dlcodus1201@naver.com>
2026-06-01 12:22:25 +0900

    [level 1] Title: 덧칠하기, Time: 0.81 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/README.md
+++ 프로그래머스/1/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989?language=java) 
 
 ### 성능 요약
 
-메모리: 68.2 MB, 시간: 5.18 ms
+메모리: 69 MB, 시간: 0.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 01일 10:53:23
+2026년 06월 01일 12:22:23
 
 ### 문제 설명
 
--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -4,24 +4,12 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         
         int count = 0;
+        int roller = 0;
         
-        int[] wall = new int[n];
-        Arrays.fill(wall, 0);
-        for (int i = 0; i < section.length; i++) {
-            wall[section[i] - 1] = 1;
-        }
-        
-        for (int i = 0; i < wall.length - m; i++) {
-            if (wall[i] == 1) {
+        for (int paint : section) {
+            if (roller <= paint) {
                 count++;
-                Arrays.fill(wall, i, i+m, 0);
-            }
-        }
-        
-        for (int i = wall.length-m; i < wall.length; i++) {
-            if (wall[i] == 1) {
-                count++;
-                break;
+                roller = paint + m;
             }
         }
         
