caad506ea8bceb51b1dceff3664f959340431c9c
이설아 <dlcodus1201@naver.com>
2026-06-05 11:05:33 +0900

    [level 1] Title: 옹알이 (2), Time: 3.88 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=java) 
 
 ### 성능 요약
 
-메모리: 64 MB, 시간: 0.44 ms
+메모리: 67 MB, 시간: 3.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 05일 10:49:48
+2026년 06월 05일 11:05:31
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,30 +1,13 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public int solution(String[] babbling) {
         
         int count = 0;
         
-        List<String> list = new ArrayList<>();
-        list.add("aya"); list.add("ye"); list.add("woo"); list.add("ma");
-        
         for (String s : babbling) {
-            StringBuilder sb = new StringBuilder();
-            String last = "last";
+            s = s.replaceAll("ayaaya|yeye|woowoo|mama", "1");
+            s = s.replaceAll("aya|ye|woo|ma", "");
             
-            for (char c : s.toCharArray()) {
-                sb.append(c);
-                
-                if (list.contains(sb.toString())) {
-                    if (sb.toString().equals(last)) break;
-                    
-                    last = sb.toString();
-                    sb.delete(0, sb.length());
-                }
-            }
-            
-            if (sb.length() == 0) count++;
+            if (s.length() == 0) count++;
         }
         
         return count;
